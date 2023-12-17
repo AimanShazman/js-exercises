6089a7ddebda65af1804f300d83fa57c97474cd1
AimanShazman <136099677+CrusineZ@users.noreply.github.com>
2023-12-17 14:56:42 +0900

    complete 08 exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,16 +10,20 @@ const sum = function(arr) {
 	return arr.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => total * num);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0) {
+    return 1;
+  } else {
+    return (num * (factorial(num - 1)));
+  }
 };
 
 // Do not edit below this line
